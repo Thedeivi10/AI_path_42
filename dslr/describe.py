6ff5659880372fc88c871525e055cd2c1eafa57b
David Gomez-Landero Lopez <d.gomezlanderolopez@gmail.com>
2026-01-08 12:16:40 +0100

    doing dslr

--- dslr/describe.py
+++ dslr/describe.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy
 import matplotlib.pyplot as plot
 import warnings
 import sys
@@ -8,8 +8,24 @@ if len(sys.argv) != 2:
     sys.exit(1)
 
 filename = sys.argv[1]
-
+if filename != 'datasets/dataset_train.csv':
+    print("The dataset for training must be called dataset_train.csv and saved in the directory \'datasets\'. Your path must be datasets/dataset_train.csv")
+    exit(1)
 try:
-    with warnings.catch_wanings():
+    with warnings.catch_warnings():
         warnings.simplefilter("error")
-        data = np.loadtxt('')
+        raw_data = numpy.genfromtxt('datasets/dataset_train.csv', delimiter= ',',
+            skip_header=1,
+            dtype=None,
+            encoding='utf-8')
+except:
+    print("Error loading dataset_train.csv")
+
+data = numpy.array(
+	[row[6:] for row in raw_data],
+	dtype = float
+)
+  
+for i,l in enumerate(data):
+    print(l);
+    if i > 4: break;
