ce6d14dab142730fe3c6a4fc6241896b4f3a6e73
David Gomez-Landero Lopez <d.gomezlanderolopez@gmail.com>
2026-01-09 12:00:09 +0100

    done describe.py of dslr and doing histogram.py. Added venv and requirements.txt to deploy venv

--- dslr/describe.py
+++ dslr/describe.py
@@ -2,6 +2,7 @@ import numpy
 import matplotlib.pyplot as plot
 import warnings
 import sys
+import math
 import pandas as pd
 
 if len(sys.argv) != 2:
@@ -21,21 +22,54 @@ except:
     exit(1)
 
 titles = raw_data.columns[6:].tolist()
-data = raw_data.iloc[:, 6:].to_numpy()
-
-def metric(function, colum):
-    for i,v in enumerate(titles):
-        if i < len(titles) - 1: print(f'{titles[i]}\t', end='')
-        else: print(titles[i])
-
+information = ["Count", "Mean", "Std", "Min", "25%", "50%", "75%", "Max"]
+raw_data_clean = raw_data.iloc[:, 6:].dropna()
+data = raw_data_clean.to_numpy()
 
 count = lambda lst: len(lst)
+mean = lambda lst: sum(lst)/len(lst)
+def std(lst):
+    m = mean(lst)
+    return math.sqrt(sum((x - m) ** 2 for x in lst) / len(lst))
 
+def min_value(lst):
+    min_value = lst[0]
+    for i, v in enumerate(lst):
+        if v < min_value: min_value = v
+    return min_value
 
-print('\t\t',end='')
-for i,v in enumerate(titles):
-    if i < len(titles) - 1: print(f'{titles[i]}\t', end='') 
-    else: print(titles[i])
+def perc25(lst):
+    sorted_list = sorted(lst)
+    return sorted_list[int(0.25 * (len(lst) - 1))]
 
-""" Count """
-print('Count\t',end='')
+def perc50(lst):
+    sorted_list = sorted(lst)
+    n = len(lst)
+    if n % 2 == 0: return ((sorted_list[n//2 - 1] + sorted_list[n//2]) / 2)
+    else: return (sorted_list[n//2])
+
+def perc75(lst):
+    sorted_list = sorted(lst)
+    return sorted_list[int(0.75 * (len(lst) - 1))]
+
+def max_value(lst):
+    sorted_list = sorted(lst)
+    return (sorted_list[-1])
+
+funciones = [count, mean, std, min_value, perc25, perc50, perc75, max_value]
+
+col_widths = []
+for title in titles:
+    col_widths.append(max(len(title), 15))
+
+print(f'{"":8}', end='')
+for i, title in enumerate(titles):
+    print(f'{title:>{col_widths[i]}}  ', end='')
+print()
+
+for i, v in enumerate(information):
+    print(f'{v:8}', end='')
+    for j in range(data.shape[1]):
+        result = funciones[i](data[:,j])
+        print(f'{result:>{col_widths[j]}.6f}  ', end='')
+    print()
